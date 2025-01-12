400e8202a60b17455e0c311c06763514b113dfbf
Harshit joshi <93515722+harshitjoshi1555@users.noreply.github.com>
2025-01-12 19:45:37 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx,css}', // Path to your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
